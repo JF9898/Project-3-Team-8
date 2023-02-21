92d31a0d2056d50da22d238026d0d92b7d799b9b
ericj <ericjyyang@gmail.com>
2023-02-21 15:38:10 -0500

    adding app.py

--- app.py
+++ app.py
@@ -1,84 +1,46 @@
-import numpy as np
-
+# Import Dependencies
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
+from flask import Flask, jsonify, render_template
+import psycopg2
+from credentials import username, password
 
-from flask import Flask, jsonify
-
-
-#################################################
 # Database Setup
-#################################################
-engine = create_engine("sqlite:///titanic.sqlite")
+engine = create_engine(f"postgresql+psycopg2://{username}:{password}@localhost:5432/soccer_db")
 
-# reflect an existing database into a new model
+# Set up a base using the existing table in the database
 Base = automap_base()
-# reflect the tables
 Base.prepare(autoload_with=engine)
+Base.classes.keys()
 
-# Save reference to the table
-Passenger = Base.classes.passenger
-
-#################################################
 # Flask Setup
-#################################################
 app = Flask(__name__)
 
-
-#################################################
 # Flask Routes
-#################################################
-
 @app.route("/")
-def welcome():
-    """List all available api routes."""
-    return (
-        f"Available Routes:<br/>"
-        f"/api/v1.0/names<br/>"
-        f"/api/v1.0/passengers"
-    )
+def index():
+    return render_template('index.html')
 
 
-@app.route("/api/v1.0/names")
-def names():
-    # Create our session (link) from Python to the DB
-    session = Session(engine)
 
-    """Return a list of all passenger names"""
-    # Query all passengers
-    results = session.query(Passenger.name).all()
+# @app.route("/api/v1.0/teams")
+# def teams():
+#     # Create our session (link) from Python to the DB
+#     session = Session(engine)
 
-    session.close()
+#     """Return a list of all team names"""
+#     # Query all teams
+#     results = session.query(Teams).all()
 
-    # Convert list of tuples into normal list
-    all_names = list(np.ravel(results))
+#     session.close()
 
-    return jsonify(all_names)
+#     # Convert list of tuples into normal list
+#     all_names = list(np.ravel(results))
 
+#     return jsonify(all_names)
 
-@app.route("/api/v1.0/passengers")
-def passengers():
-    # Create our session (link) from Python to the DB
-    session = Session(engine)
-
-    """Return a list of passenger data including the name, age, and sex of each passenger"""
-    # Query all passengers
-    results = session.query(Passenger.name, Passenger.age, Passenger.sex).all()
-
-    session.close()
-
-    # Create a dictionary from the row data and append to a list of all_passengers
-    all_passengers = []
-    for name, age, sex in results:
-        passenger_dict = {}
-        passenger_dict["name"] = name
-        passenger_dict["age"] = age
-        passenger_dict["sex"] = sex
-        all_passengers.append(passenger_dict)
-
-    return jsonify(all_passengers)
 
 
 if __name__ == '__main__':
